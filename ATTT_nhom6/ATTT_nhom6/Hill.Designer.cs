576e54e42c5656269359d026460bc652c1fca841
thuonghb <tienthuongpham@gmail.com>
2024-11-07 21:55:30 +0700

    Add files via upload

---
+++ ATTT_nhom6/ATTT_nhom6/Hill.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace ATTT_nhom6
+{
+    partial class Hill
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mahoa = new System.Windows.Forms.Button();
+            this.giaima = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.sizekey = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // mahoa
+            // 
+            this.mahoa.Enabled = false;
+            this.mahoa.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mahoa.Location = new System.Drawing.Point(633, 195);
+            this.mahoa.Name = "mahoa";
+            this.mahoa.Size = new System.Drawing.Size(189, 40);
+            this.mahoa.TabIndex = 3;
+            this.mahoa.Text = "Mã hóa";
+            this.mahoa.UseVisualStyleBackColor = true;
+            // 
+            // giaima
+            // 
+            this.giaima.Enabled = false;
+            this.giaima.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.giaima.Location = new System.Drawing.Point(633, 353);
+            this.giaima.Name = "giaima";
+            this.giaima.Size = new System.Drawing.Size(189, 40);
+            this.giaima.TabIndex = 4;
+            this.giaima.Text = "Giải mã";
+            this.giaima.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 26);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Bản rõ:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 26);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Khóa:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 359);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 26);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Bản mã:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(36, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(224, 26);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Nhập độ lớn của khóa:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(300, 19);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(177, 34);
+            this.textBox4.TabIndex = 9;
+            // 
+            // sizekey
+            // 
+            this.sizekey.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sizekey.Location = new System.Drawing.Point(633, 76);
+            this.sizekey.Name = "sizekey";
+            this.sizekey.Size = new System.Drawing.Size(189, 40);
+            this.sizekey.TabIndex = 10;
+            this.sizekey.Text = "Bắt đầu";
+            this.sizekey.UseVisualStyleBackColor = true;
+            this.sizekey.Click += new System.EventHandler(this.sizekey_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(524, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(177, 34);
+            this.textBox2.TabIndex = 13;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(199, 88);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(365, 305);
+            this.panel1.TabIndex = 14;
+            // 
+            // Hill
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 456);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.sizekey);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.giaima);
+            this.Controls.Add(this.mahoa);
+            this.Name = "Hill";
+            this.Text = "Hill";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button mahoa;
+        private System.Windows.Forms.Button giaima;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button sizekey;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
